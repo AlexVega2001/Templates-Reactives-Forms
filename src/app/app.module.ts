80cb2a5b71cf13c41eb3a6205e385f174ea481b9
Alexander Vega <87383623+AlexVega2001@users.noreply.github.com>
2023-05-17 00:28:35 -0500

    Se agregó la pantalla de registro de usuario y sus respectiva validaciones

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http'
+
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 
@@ -10,8 +11,9 @@ import { SharedModule } from './shared/shared.module';
     AppComponent
   ],
   imports: [
-    BrowserModule,
     AppRoutingModule,
+    BrowserModule,
+    HttpClientModule,
     SharedModule
   ],
   providers: [],
